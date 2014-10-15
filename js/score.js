dea493bf39adac5b6181aaf42b58e7d77c251e79
Norman Walsh <ndw@nwalsh.com>
2014-10-14 20:43:40 -0500

    First 'finished' version.

--- js/score.js
+++ js/score.js
@@ -1,7 +1,8 @@
 // According to the rules at http://www.pagat.com/tarot/frtarot.html
 
 var pointsTakerNeeds = [56, 51, 41, 36];
-var totalCards = 21 + (4 * 14);
+var totalCards = 21 + (4 * 14) + 1;
+var score;
 
 function scoreTarot() {
     var bouts = parseInt($("#bouts").val(), 10);
@@ -13,8 +14,26 @@ function scoreTarot() {
     var poignee = parseInt($("#poignee").val(), 10);
     var bid = parseInt($("#bid").val(), 10);
     var petitaubout = parseInt($("#petitaubout").val(), 10);
-    var cardPoints, pointsNeeded, cardCount, score, scoreMsg;
+    var cardPoints, pointsNeeded, cardCount, scoreMsg, cardCount;
+    var player;
     var pt, pb, pg, ch, mu;
+
+    say("cardmsg", "");
+    say("message", "");
+
+    cardCount = bouts + roi + dame + cavalier + valet + cards;
+
+    // N.B. Although there are four cards per hand, we can't check
+    // that the total number of cards is a multiple of four because
+    // there are six extra cards that have been set aside. The best
+    // we can do is make sure that the number is even and not in
+    // excess of the total number of cards.
+
+    if (cardCount > totalCards) {
+        say("cardmsg", "That's too many cards!");
+    } else if ((cardCount % 2 != 0) && (cards != 0)) {
+        say("cardmsg", "Count again, invalid card count");
+    }
 
     pointsNeeded = pointsTakerNeeds[bouts];
 
@@ -32,16 +51,18 @@ function scoreTarot() {
     cardCount = bouts + roi + dame + cavalier + valet + cards;
 
     ch = 0;
-    $("#ex-chelem-succ").html("failed");
+    $("#ex-chelem-succ").html("and failed");
     $("#ex-chelem-decl").html("did not declare");
+
     if (cardCount > totalCards) {
-        message("That's too many cards!");
+        // nop
     } else if (cardCount === totalCards) {
-        $("#ex-chelem-succ").html("succeeded");
         if ($("#chelem").val() === "annonce") {
+            $("#ex-chelem-succ").html("and succeeded");
             $("#ex-chelem-decl").html("declared");
             ch = 400;
         } else {
+            $("#ex-chelem-succ").html("but succeeded");
             ch = 200;
         }
     } else {
@@ -51,12 +72,14 @@ function scoreTarot() {
         }
     }
 
-    score = ((25 + pt + pb) * mu) + pg + ch;
+    score = ((25 + Math.abs(pt) + pb) * mu) + pg + ch;
+
+    player = $("#player :selected").text();
 
     if (pt >= 0) {
-        scoreMsg = "You win " + score + " points from <em>each</em> player"
+        scoreMsg = player + " wins " + score + " points from <em>each</em> player"
     } else {
-        scoreMsg = "You <em>lose</em> " + Math.abs(score)
+        scoreMsg = player + " <em>loses</em> " + Math.abs(score)
                    + " points to <em>each</em> player"
     }
 
@@ -110,7 +133,7 @@ function scoreTarot() {
         $("#ex-bid-p").show();
     }
 
-    $("#ex-pt").html(pt);
+    $("#ex-pt").html(Math.abs(pt));
     $("#ex-pb").html(pb);
     $("#ex-mu").html(mu);
     $("#ex-pg").html(pg);
@@ -120,16 +143,94 @@ function scoreTarot() {
     $("#ex-score").html(score);
 
     $("#explain").html(
-        "((25 + (" + cardPoints + " - " + pointsNeeded + ") + " + pb + ") * " + mu + " ) + " + pg + " + " + ch
+        "((25 + abs(" + cardPoints + " - " + pointsNeeded + ") + " + pb + ") * " + mu + " ) + " + pg + " + " + ch
     );
 }
 
-function message(msg) {
-        $("#message").html("That's too many cards.");
+function playerName() {
+    var pos = 1;
+    var name;
+
+    $("input.playername").each(function () {
+        name = $(this).val();
+        $("#player option[value='" + pos + "']").text(name)
+        pos++;
+    });
+
+    updatePlayer();
+}
+
+function updatePlayer() {
+    var player;
+
+    player = $("#player :selected").text();
+
+    $(".ex-current-player").each(function () {
+        $(this).html(player);
+    });
+
+    scoreTarot();
+}
+
+function toggleDisplay() {
+    var id, pos, display;
+
+    id = $(this).attr("id");
+    pos = id.indexOf("-");
+    id = id.substring(0, pos) + "-div";
+
+    display = $("#" + id).css("display");
+    if (display === "none") {
+        $("#" + id).css("display", "block");
+    } else {
+        $("#" + id).css("display", "none");
+    }
+}
+
+function recordScore() {
+    var pid, pos, id, curScore;
+
+    pid = parseInt($("#player :selected").val(), 10);
+
+    for (pos = 1; pos < 5; pos++) {
+        id = "#player" + pos + "score";
+        curScore = parseInt($(id).html(), 10);
+
+        if (pos === pid) {
+            curScore = curScore + (score * 3);
+        } else {
+            curScore = curScore - score;
+        }
+
+        $(id).html(curScore);
+    }
+
+    $("#bouts").val(0);
+    $("#roi").val(0);
+    $("#dame").val(0);
+    $("#cavalier").val(0);
+    $("#valet").val(0);
+    $("#cards").val(0);
+    $("#poignee").val(0);
+    $("#bid").val(1);
+    $("#petitaubout").val(0);
+
+    say("cardmsg", "");
+    say("score", "");
+    say("message", "");
+}
+
+function say(id, html) {
+    $("#"+id).html(html);
 }
 
 $(document).ready(function() {
-    $("select").change(scoreTarot);
-    $("input").change(scoreTarot);
+    $("select.score").change(scoreTarot);
+    $("select.player").change(updatePlayer);
+    $("input.playername").change(playerName);
+    $("input.score").change(scoreTarot);
+    $("input.record").click(recordScore);
+    $("input.button").click(toggleDisplay);
+    updatePlayer();
     scoreTarot();
 });
